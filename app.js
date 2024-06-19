b8725277a6c2742e2088a6a9f46ba4f5d6db460e
hoagbaodinh <46434823+hoagbaodinh@users.noreply.github.com>
2024-06-19 12:12:29 +0700

    up

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const moviesRouter = require("./routes/movie");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const moviesRouter = require('./routes/movie');
+const http = require('http');
+const cors = require('cors');
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ['http://localhost:3000', 'https://movie-client-zkmn.onrender.com'],
+    credentials: true,
+  })
+);
 
 app.use(bodyParser.json());
 
@@ -12,9 +18,10 @@ app.use(moviesRouter);
 
 app.use((error, req, res, next) => {
   const status = error.status || 500;
-  const message = error.message || "Something went wrong.";
+  const message = error.message || 'Something went wrong.';
   console.log(status, message);
   res.status(status).json({ message: message });
 });
+const server = http.createServer(app);
 
-app.listen(5050);
+server.listen(5050);
